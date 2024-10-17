7ddb095a10efc1d3f7423dda65b927b0cb4885b3
liche-m <moodleyliche@gmail.com>
2024-10-17 12:26:14 +0200

    fix: deleted

--- lambda_tools/versions_springcleaner/data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_caller_identity" "this" {}
-
-data "aws_region" "current" {}
-
-locals {
-  account_id = data.aws_caller_identity.this.account_id
-  aws_region = data.aws_region.current.name
-  image_uri  = "${local.account_id}.dkr.ecr.${local.aws_region}.amazonaws.com/infra/versions-springcleaner:${var.image_tag}"
-}
